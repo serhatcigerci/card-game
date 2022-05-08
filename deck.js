37e23cd39380462e49c4836023fe92b7a86ff4d1
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-08 21:55:51 +0300

    getting fresh deck

--- deck.js
+++ deck.js
@@ -1,3 +1,6 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
 class Deck {
   constructor(cards) {
     this.cards = cards
@@ -9,4 +12,12 @@ class Card {
     this.suit = suit
     this.value = value    
   }
+}
+
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
+    })
+  })
 }
