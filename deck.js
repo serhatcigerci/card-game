8c8c592ae1f0a7ebb32f892a468d3033be498fb3
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-09 11:48:03 +0300

    shuffling the deck

--- deck.js
+++ deck.js
@@ -6,8 +6,17 @@ export default class Deck {
     this.cards = cards
   }
 
+  get numberOfCards() {
+    return this.cards.length
+  }
+
   shuffle() {
-    this.cards.sort((a, b) => Math.random() - 0.5)
+   for (let i = this.numberOfCards - 1; i > 0; i--) {
+     const newI = Math.floor(Math.random() * (i + 1))
+     const oldValue = this.cards[newI]
+     this.cards[newI] = this.cards[i]
+     this.cards[i] = oldValue
+   }
   }
 }
 
