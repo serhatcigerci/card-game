4835778508bcaae30c546696217358a4f0ee2c1d
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-10 16:08:20 +0300

    card flip and pop

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ const computerDeckElem = document.querySelector('.computer-deck')
 const playerDeckElem = document.querySelector('.player-deck')
 const text = document.querySelector('.text')
 
-let playerDeck, computerDeck
+let playerDeck, computerDeck, inRound
+
+
+document.addEventListener('click', () => {
+  if (inRound) {
+    cleanBeforeRound()
+  } else {
+    flipCards()
+  }
+})
 
 startGame()
 function startGame() {
@@ -16,6 +25,8 @@ function startGame() {
   const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
   playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
   computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+  inRound = false
+  
   cleanBeforeRound()
 }
 
@@ -23,8 +34,19 @@ function cleanBeforeRound() {
   computerCardSlot.innerHTML = ''
   playerCardSlot.innerHTML = ''
   text.innerText = ''
+  inRound = false
 
   updateDeckCount()
+}
+
+function flipCards() {
+  inRound = true
+
+  const playerCard = playerDeck.pop()
+  const computerCard = computerDeck.pop()
+
+  playerCardSlot.appendChild(playerCard.getHTML())
+  computerCardSlot.appendChild(computerCard.getHTML())
 }
 
 function updateDeckCount() {
